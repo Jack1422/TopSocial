ec05653dd50dc952dd0d6189ecf8d43d283619fc
landy <landy_good@hotmail.com>
2024-04-29 21:35:42 +1000

    Add top social

---
+++ backend/routes/profile.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const {getProfile} = require('../controllers/profileController')
+const profileRouter = express.Router();
+
+
+profileRouter.get('/',getProfile)
+
+module.exports = profileRouter;
